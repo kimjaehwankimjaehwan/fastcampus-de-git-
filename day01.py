364bcb743c1d85d5c8e8b18efae2ae71eec947a8
kimjaehwankimjaehwan <kjh861213@gmail.com>
2024-08-03 07:33:12 +0900

    for twice

--- day01.py
+++ day01.py
@@ -17,3 +17,14 @@ print(np.random.randint(1,100))
 print("hello, welcom to the world")
 
 print(np.random.randint(1,100))
+
+for i in range(10)
+    print("wowowowowowo")
+
+# fc2b63cb5bfb79e75df3ed1430dc13c1529a164d
+# 4908a5d68287108d36f6856ed58250161f0f9be4
+
+for i in range(10)
+    print("wowowowowowo")
+for i in range(10)
+    print("wowowowowowo")
