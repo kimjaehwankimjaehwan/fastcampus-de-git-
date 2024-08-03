4ee9dd336d5500de6644bd29e40d51f9fb12a890
kimjaehwankimjaehwan <kjh861213@gmail.com>
2024-08-03 17:32:35 +0900

    lecture4

---
+++ dataanalysis/dataexer.py
@@ -0,0 +1,35 @@
+# dataexer.py
+# https://jsonplaceholder.typicode.com/todos
+
+import requests
+import json
+import pandas as pd
+import numpy as np
+
+def p(data):
+    print(data, '\n')
+
+# 1
+response = requests.get('https://jsonplaceholder.typicode.com/todos')
+data = response.json()
+print(data)
+
+df = pd.DataFrame(data)
+print(df)
+
+resultdf = df.assign(result = np.where(df['userId']>=5,"pass",'fail'))
+print(resultdf)
+
+# 2
+result = resultdf.query("userId>=5")[['userId', 'id', 'title', 'completed']]
+print(result)
+
+# 3
+result = result.sort_values("userId", ascending=False)
+print(result)
+
+# 4
+result.to_csv('result.csv', index = False)
+
+
+
